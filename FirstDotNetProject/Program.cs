3aea07e1189883678878edd847cd9a91a6a55212
kajo2001 <kajalbhalerao75@gmail.com>
2026-05-12 12:44:42 +0530

    Add project files.

---
+++ FirstDotNetProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDotNetProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int sum = a + b;
+            Console.WriteLine("Addition is : " + sum);
+
+        }
+    }
+}
